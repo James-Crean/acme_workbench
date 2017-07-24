b6964e96be3236f5d53bd0f76db80937fddbe82a
Sterling Baldwin <baldwin32@llnl.gov>
2017-07-24 15:35:17 -0700

    Adds test for index

--- workbench-backend/index/tests.py
+++ workbench-backend/index/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+    """
+    Tests for the Index app views
+    """
+    def test_get_index(self):
+        """
+        Test that the index page returns success
+        """
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
