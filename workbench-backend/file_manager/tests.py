f8b743f1407878379791529fd5124ea50c585fde
Sterling Baldwin <baldwin32@llnl.gov>
2017-08-02 16:38:20 -0700

    Adds the first few file_manager views, models, tests

--- workbench-backend/file_manager/tests.py
+++ workbench-backend/file_manager/tests.py
@@ -1,3 +1,72 @@
+import json
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class FileManagerViewTests(TestCase):
+    """
+    Tests for the Index app views
+    """
+    fixtures = ['seed.json']
+
+    def test_data_set_list(self):
+        """
+        Test '/file_manager/get_data_set_list/'
+        """
+        expected_result = ['dummy set']
+        client = Client()
+        client.login(
+            username='test_user',
+            password='qwertyuiop')
+
+        res = client.get('/file_manager/get_data_set_list/')
+        self.assertEqual(res.status_code, 200)
+
+        data = json.loads(res.content)
+        self.assertTrue(data == expected_result)
+
+    def test_get_data_set(self):
+        """
+        test '/file_manager/get_data_set/'
+        """
+        expected_result = {
+            "file_list": ["dummy_1", "dummy_2"],
+            "allowed_access": ["test_user"],
+            "name": "dummy set",
+            "metadata": "This is a dummy data set"
+        }
+        client = Client()
+        client.login(
+            username='test_user',
+            password='qwertyuiop')
+
+        res = client.get('/file_manager/get_data_set/', {'data_set_name': 'dummy set'})
+        self.assertEqual(res.status_code, 200)
+
+        data = json.loads(res.content)
+        self.assertTrue(data == expected_result)
+
+    def test_get_file_info(self):
+        """
+        test '/file_manager/get_file_info/
+        """
+        expected_result = {
+            'owner': 'test_user',
+            'path': '/some/path/dummy_1.nc',
+            'display_name': 'dummy_1',
+            'data_type': 1,
+            'allowed_access': ['test_user']
+        }
+        client = Client()
+        client.login(
+            username='test_user',
+            password='qwertyuiop')
+
+        params = {
+            'data_set_name': 'dummy set',
+            'data_file_name': 'dummy_1'
+        }
+        res = client.get('/file_manager/get_file_info/', params)
+        self.assertEqual(res.status_code, 200)
+        data = json.loads(res.content)
+        self.assertTrue(data == expected_result)
--- workbench-backend/file_manager/views.py
+++ workbench-backend/file_manager/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .models import DataFile, DataSet, DataType
+
+def get_data_set_list(request):
+    """
+    Return a list of all data sets the requesting user has access to
+
+    parameters:
+        request (WSGIrequest)
+
+    return:
+        json encoded list of DataSet names
+    """
+    user = request.user
+    if not user.is_authenticated():
+        return HttpResponse(status=401)
+
+    site_user = User.objects.get(username=user.username)
+    data_sets = [data_set.toDict()['name'] for data_set in DataSet.objects.filter(allowed_access=site_user.id)]
+    response = json.dumps(data_sets)
+    return HttpResponse(response)
+
+def get_data_set(request):
+    """
+    Gets all info about a given dataset
+
+    parameters:
+        request.GET['data_set'] (str) the name of the dataset to get the info for
+
+    returns:
+        json encoded DataSet
+    """
+    user = request.user
+    if not user.is_authenticated():
+        return HttpResponse(status=401)
+    site_user = User.objects.get(username=user)
+
+    try:
+        data_set_name = request.GET['data_set_name']
+    except Exception as e:
+        return HttpResponse(status=402)
+
+    data_set = DataSet.objects.filter(allowed_access=site_user.id)
+    if not data_set:
+        return HttpResponse(json.dumps({}))
+    response = json.dumps(data_set[0].toDict())
+    return HttpResponse(response)
+
+def get_file_info(request):
+    user = request.user
+    if not user.is_authenticated():
+        return HttpResponse(status=401)
+    site_user = User.objects.get(username=user)
+
+    try:
+        data_set_name = request.GET['data_set_name']
+        data_file_name = request.GET['data_file_name']
+    except Exception as e:
+        return HttpResponse(status=402)
+
+    data_set = DataSet.objects.get(allowed_access=site_user.id, name=data_set_name)
+    if not data_set:
+        return HttpResponse(json.dumps({}))
+
+    data_file = data_set.file_list.get(display_name=data_file_name)
+    if not data_file:
+        return HttpResponse(json.dumps({}))
+
+    response = json.dumps(data_file.toDict())
+    return HttpResponse(response)
