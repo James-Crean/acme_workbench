8bd36212b752e47fa5a505ab78fe8c409863f242
Crean <crean2@llnl.gov>
2017-08-04 14:37:13 -0700

    Adds back button to dataset-view component. Adds a bit of css.

--- workbench-frontend/src/app/dataset-view/dataset-view.component.ts
+++ workbench-frontend/src/app/dataset-view/dataset-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FileService } from '../file.service'
 
 @Component({
@@ -12,8 +12,13 @@ export class DatasetViewComponent implements OnInit {
   file_list: Object[];
   @Input() datasetName: string;
   @Input() dataset: any;
+  @Output() showDatasetCollection: EventEmitter<string> = new EventEmitter<string>();
 
   constructor(private fileService: FileService) { }
 
   ngOnInit() { }
+
+  go_back(){
+    this.showDatasetCollection.emit();
+  }
 }
