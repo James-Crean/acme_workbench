3ee56503802f1af3fd6736eeac8bc39d49a0dfb9
Sterling Baldwin <baldwin32@llnl.gov>
2017-08-04 10:18:10 -0700

    Adds the start of the file-service

---
+++ workbench-frontend/src/app/file-service.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { DataFile } from './data-file'
+import { DataSet } from './data-set'
+
+@Injectable()
+export class FileServiceService {
+
+  private getDataSetListUrl = '/file_manager/get_data_set_list/';
+  private getDataSetUrl = '/file_manager/get_data_set/';
+  private getFileInfoUrl = '/file_manager/get_file_info/';
+
+  constructor(private http: Http) { }
+
+  getDataSetList(): Observable<string[]>{
+    return this.http.get(this.getDataSetListUrl)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  getDataSet(): Observable<DataSet>{
+    return this.http.get(this.getDataSetUrl)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  getFileInfo(): Observable<DataFile>{
+    return this.http.get(this.getFileInfoUrl)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response){
+    let body = res.json();
+    console.log(body);
+    return body || {};
+  }
+
+  private handleError(error: Response | any){
+    console.log('ERROR');
+    console.log(error.toString())
+    return Observable.throw(error.toString());
+  }
+}
