c4a2819340117f7045df22acc4268c46cd9be9c9
Sterling Baldwin <baldwin32@llnl.gov>
2017-08-09 16:12:09 -0700

    Working on CRUD

--- workbench-frontend/src/app/app.module.ts
+++ workbench-frontend/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { MaterializeModule } from "angular2-materialize";
+import { FileDropDirective, FileSelectDirective } from 'ng2-file-upload';
+import { CookieModule } from 'ngx-cookie';
 
 import { AppComponent } from './app.component';
 import { DataManagerComponent } from './data-manager/data-manager.component';
@@ -13,6 +16,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SettingsComponent } from './settings/settings.component';
 import { DatasetCollectionComponent } from './dataset-collection/dataset-collection.component';
 import { DatasetViewComponent } from './dataset-view/dataset-view.component';
+import { DataImportComponent } from './data-import/data-import.component';
+import { DatasetDeleteComponent } from './dataset-delete/dataset-delete.component';
 
 @NgModule({
   declarations: [
@@ -24,12 +29,18 @@ import { DatasetViewComponent } from './dataset-view/dataset-view.component';
     SidebarComponent,
     SettingsComponent,
     DatasetCollectionComponent,
-    DatasetViewComponent
+    DatasetViewComponent,
+    DataImportComponent,
+    FileDropDirective, 
+    FileSelectDirective, 
+    DatasetDeleteComponent
   ],
   imports: [
     BrowserModule,
     MaterializeModule,
-    HttpModule
+    HttpModule,
+    FormsModule,
+    CookieModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
