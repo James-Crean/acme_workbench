9cf7705ff7893b00fc62a7449d200b46dd651aa1
Sterling Baldwin <baldwin32@llnl.gov>
2017-08-10 09:17:17 -0700

    Adds stripped version of the local_settings.py for travis

---
+++ workbench-backend/local_settings.py
@@ -0,0 +1,9 @@
+cliendID = ''
+secret = ''
+
+esgf_slcs_serve = ''
+# authorize_url = "{}/esgf-slcs/oauth/authorize".format(esgf_slcs_server)
+# token_url = "{}/esgf-slcs/oauth/access_token".format(esgf_slcs_server)
+# certificate_url = "{}/esgf-slcs/oauth/certificate/".format(esgf_slcs_server)
+
+userdata_storage_path = '/Users/baldwin32/projects/acme_workbench/userdata/'
