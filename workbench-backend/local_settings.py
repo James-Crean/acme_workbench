85e4551d4599fcd936e61530bf1cf5a96aa8cd6f
Sterling Baldwin <baldwin32@llnl.gov>
2017-08-10 14:40:46 -0700

    Adds check for travis environment

--- workbench-backend/local_settings.py
+++ workbench-backend/local_settings.py
@@ -5,5 +5,9 @@ esgf_slcs_serve = ''
 # authorize_url = "{}/esgf-slcs/oauth/authorize".format(esgf_slcs_server)
 # token_url = "{}/esgf-slcs/oauth/access_token".format(esgf_slcs_server)
 # certificate_url = "{}/esgf-slcs/oauth/certificate/".format(esgf_slcs_server)
+import os
 
-userdata_storage_path = '/Users/baldwin32/projects/acme_workbench/userdata/'
+if os.environ.get('TRAVIS') == 'true':
+    userdata_storage_path = '.'
+else:
+    userdata_storage_path = '/Users/baldwin32/projects/acme_workbench/userdata/'
