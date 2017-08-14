58819a9bc2565ca0ed7581ff18a8185dc97f7f6f
Crean <crean2@llnl.gov>
2017-08-14 13:45:35 -0700

    Updates filepermission modal to change add/remove lists without closing

--- workbench-frontend/src/app/file-permissions-modal/file-permissions-modal.component.ts
+++ workbench-frontend/src/app/file-permissions-modal/file-permissions-modal.component.ts
@@ -34,24 +34,33 @@ export class FilePermissionsModalComponent {
    }
 
   openModal(datafile: DataFile){ 
+    this.datafile = datafile;
     this.fileService.getUserList()
     .subscribe(
       users => {
+        var newuserlist = [];
         let index = users.indexOf(datafile.owner);
         if (index > -1) { 
             users.splice(index, 1); //remove owner from list of users
         }
-        this.user_list = users;
+        for(let count=0; count < users.length; count++){
+          if(!this.datafile.allowed_access.includes(users[count])){
+            newuserlist.push(users[count]);
+            //make a userlist of only users without access already.
+          }
+        }
+        this.user_list = newuserlist;
       },
       error => {
         this.errorMessage = <any>error;
       }
     );
-    this.datafile = datafile;
     this.modalActions.emit({action:"modal", params:['open', { dismissible: true }]});
   }
 
   closeModal() {
+    this.addSelectedUsers = [];
+    this.removeSelectedUsers = []; 
     this.modalActions.emit({action:"modal", params:['close']});
   }
 
@@ -60,6 +69,16 @@ export class FilePermissionsModalComponent {
     this.fileService.addFilePermissions(this.addSelectedUsers, this.datafile.id, csrf)
       .subscribe(
         success => {
+          for(let user of this.addSelectedUsers){
+            let index = this.user_list.indexOf(user);
+            if (index > -1) { 
+              this.datafile.allowed_access.push(this.user_list[index]);
+              this.datafile.allowed_access = this.datafile.allowed_access.slice(); //slice copies the array. we do this to trigger angular update 
+              this.user_list.splice(index, 1);
+              //After granting a user permission, remove them from the "Add Permissions" selectbox
+            }
+          }
+          this.addSelectedUsers = []; //Reset the select box on success
           this.toastService.toast('Permissions successfully added');
           this.updatePermissions.emit(this.datafile.display_name);
         },
@@ -75,7 +94,19 @@ export class FilePermissionsModalComponent {
     this.fileService.removeFilePermissions(this.removeSelectedUsers, this.datafile.id, csrf)
       .subscribe(
         success => {
-          this.toastService.toast('Permissions successfully removed');
+          for(let user of this.removeSelectedUsers){
+            let index = this.datafile.allowed_access.indexOf(user);
+            if (index > -1) { 
+              //add the removed user to the pool of users that can be given permission
+              this.user_list.push(this.datafile.allowed_access[index]);
+              this.user_list = this.user_list.slice(); //slice copies the array. we do this to trigger angular update 
+              this.datafile.allowed_access.splice(index, 1);
+              //After removing a user's permissions, remove them from the "remove permissions" selectbox
+            }
+          }
+
+          this.removeSelectedUsers = []; //Reset the select box on success
+          this.toastService.toast('Permissions successfully removed'); //Toast for the user so they know it worked
           this.updatePermissions.emit(this.datafile.display_name);
         },
         error => {
