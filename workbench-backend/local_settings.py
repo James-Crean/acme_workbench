c22fc9219e4bbb285f53772ddc693a01ceee933f
Sterling Baldwin <baldwin32@llnl.gov>
2017-08-23 15:59:59 -0700

    Adds info into local_settings for globus auth

--- workbench-backend/local_settings.py
+++ workbench-backend/local_settings.py
@@ -1,13 +1,12 @@
-cliendID = ''
-secret = ''
+client_id = '...'
+secret = '...'
 
-esgf_slcs_serve = ''
-# authorize_url = "{}/esgf-slcs/oauth/authorize".format(esgf_slcs_server)
-# token_url = "{}/esgf-slcs/oauth/access_token".format(esgf_slcs_server)
-# certificate_url = "{}/esgf-slcs/oauth/certificate/".format(esgf_slcs_server)
-import os
+esgf_slcs_server = 'pcmdi11.llnl.gov'
+authorize_url = "{}/esgf-slcs/oauth/authorize".format(esgf_slcs_server)
+token_url = "{}/esgf-slcs/oauth/access_token".format(esgf_slcs_server)
+certificate_url = "{}/esgf-slcs/oauth/certificate/".format(esgf_slcs_server)
+redirect_url = 'http://localhost:8000/oauth_calback'
 
-if os.environ.get('TRAVIS') == 'true':
-    userdata_storage_path = '.'
-else:
-    userdata_storage_path = '/Users/' + os.environ['USER'] + '/projects/acme_workbench/userdata/'
+globus_client_id = '...'
+globus_client_secret = '...'
+globus_redirect_url = '...'
