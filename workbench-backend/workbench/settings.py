e29995b65c171ec7e07befe4eedeffafcd2bbbc5
Sterling Baldwin <baldwin32@llnl.gov>
2017-08-23 16:01:15 -0700

    Moves travis env check to the settings

--- workbench-backend/workbench/settings.py
+++ workbench-backend/workbench/settings.py
@@ -12,27 +12,28 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
+ 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '075=aws3sw0@kd09eknq==br&=stl4+)(34@wd!i44&xjam0w6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '128.15.244.229']
 
 APPEND_SLASH = True
 
 # Application definition
 
 STATICFILES_DIRS = ("static",)
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
