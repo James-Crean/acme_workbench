9a734344138ff3fbeb10c8cbff44c75512fdc2df
Crean <crean2@llnl.gov>
2017-08-25 15:05:12 -0700

    Removes context menu in favor of materialize dropdown.

--- workbench-frontend/src/app/dataset-view/dataset-view.component.ts
+++ workbench-frontend/src/app/dataset-view/dataset-view.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ViewChild } from '@angular/core';
-
-import { ContextMenuModule, ContextMenuComponent, ContextMenuService } from 'ngx-contextmenu'
+import {MaterializeDirective, MaterializeAction} from "angular2-materialize";
 
 import { FilePermissionsModalComponent } from '../file-permissions-modal/file-permissions-modal.component' 
 import { FileService } from '../file.service'
@@ -16,23 +15,23 @@ import { DataFile } from '../data-file'
 export class DatasetViewComponent implements OnInit {
 
   @ViewChild(FilePermissionsModalComponent)
-  @ViewChild('fileMenu') public fileMenu: ContextMenuComponent;
   private filePermissionsModal: FilePermissionsModalComponent;
   file_list: Object[];
   fileInfoList: DataFile[];
   currentFileIndex: number;
   errorMessage: string;
-  @Input() contextMenu: ContextMenuComponent;
   @Input() dataset: any;
   @Output() showDatasetCollection: EventEmitter<string> = new EventEmitter<string>();
+  dropdownActions = new EventEmitter<string|MaterializeAction>();
+  currentDropdownIndex: number;
 
-  constructor(private fileService: FileService, private contextMenuService: ContextMenuService) { }
+  constructor(private fileService: FileService) { }
 
   ngOnInit() {
     this.fileInfoList = new Array(this.dataset.file_list.length);
   }
 
-  getFileInfo(i){
+  getFileInfo(i, callback = undefined){
     if(this.fileInfoList[i]){
       return;
     }
@@ -48,6 +47,9 @@ export class DatasetViewComponent implements OnInit {
           data_type: <number>file.data_type
         });
         this.fileInfoList[i] = newFile;
+        if(callback){
+          callback();
+        }
       },
       error => {
         this.errorMessage = <any>error;
@@ -82,18 +84,27 @@ export class DatasetViewComponent implements OnInit {
       });
   }
 
-  public openMenu($event: MouseEvent, item: any): void {
-    console.log(this.fileMenu)
-    this.contextMenuService.show.next({
-      contextMenu: this.fileMenu,
-      event: $event,
-      item: item,
-    });
-    $event.preventDefault();
-    $event.stopPropagation();
+  setIndex(i: number){
+    console.log("preparing index: ", i);
+    this.currentDropdownIndex = i;
   }
   
-  sendToVis(i: any){
-    console.log(i)
+  //1. When the user clicks the dropdown for "Send to visualization" an event is passed.
+  //2. We stop event propagation to prevent the collapsible from triggering 
+  //3. We manually close the dropdown since it will not receive the bubbled event
+  //4. Make sure the fileInfo has been loaded from the server
+  //5. If not, we get the file info, and pass a callback function.
+  sendToVis($event?){ 
+    if($event){ //1
+      $event.stopPropagation(); //2
+      this.dropdownActions.emit({action:"dropdown", params:['close']}); //3
+    }
+    if(this.fileInfoList[this.currentDropdownIndex]){ //4 
+      console.log("Content is loaded. free to send");
+    }
+    else{ //5 
+      console.log("no data yet, fetching....")
+      this.getFileInfo(this.currentDropdownIndex, this.sendToVis.bind(this))
+    }
   }
 }
